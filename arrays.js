73b9287321addebe34bd149ff7b633f7f2a71926
Sujata <sujataj0999@gmail.com>
2025-11-17 20:53:41 +0530

    array

---
+++ arrays.js
@@ -0,0 +1,21 @@
+const myArray = ["banana", "apple", "grapes", "orange" ,"kiwi", "watermelon"];
+//console.log(myArray[0]);
+const myArray2 = new Array(10, 12, 14, 16,)
+
+//Array Methods
+// myArray.push("mango");
+// myArray.pop()
+// myArray.shift()
+// myArray.unshift("jamun");
+// console.log(myArray.includes("grapes"));
+// console.log(myArray.indexOf("grapes"));
+// console.log(myArray);
+
+//slice and splice
+console.log("A", myArray);
+const myn1 = myArray.slice(1,3);
+console.log(myn1);
+console.log("B", myArray);
+const myn2 = myArray.splice(1,3);
+console.log("C", myArray);
+console.log(myn2);
