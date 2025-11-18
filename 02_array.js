2ee9caaf7b7ac34a73da93480e82617b260fe215
Sujata <sujataj0999@gmail.com>
2025-11-18 20:14:57 +0530

    add

--- 02_array.js
+++ 02_array.js
@@ -3,13 +3,15 @@ const dc_heros = ["superman", "flash", "batman"]
 // marvel_heros.push(dc_heros)
 // console.log(marvel_heros);
 
-new_heros = marvel_heros.concat(dc_heros)
-console.log(new_heros);
+// new_heros = marvel_heros.concat(dc_heros)
+// console.log(new_heros);
 
-const all_new_heros = [...marvel_heros,...dc_heros]
-console.log(all_new_heros);
+// const all_new_heros = [...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
 
 const another_array = ["banana","orange", "apple", ['kiwi', 'grapes',], "watermelon", ['potato', 'onion', ['sweet', 'laddu']]]
 
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array)
+
+console.log(Array.from("sujata"))
