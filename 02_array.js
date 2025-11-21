37f3df9b585d3fa5f8a2ec27b6b9d5458bfa2167
Sujata <sujataj0999@gmail.com>
2025-11-21 10:34:59 +0530

    new objects method

--- 02_array.js
+++ 02_array.js
@@ -1,5 +1,5 @@
-const marvel_heros = ["thor", "Ironman", "spiderman"];
-const dc_heros = ["superman", "flash", "batman"]
+// const marvel_heros = ["thor", "Ironman", "spiderman"];
+// const dc_heros = ["superman", "flash", "batman"]
 // marvel_heros.push(dc_heros)
 // console.log(marvel_heros);
 
@@ -9,9 +9,26 @@ const dc_heros = ["superman", "flash", "batman"]
 // const all_new_heros = [...marvel_heros,...dc_heros]
 // console.log(all_new_heros);
 
-const another_array = ["banana","orange", "apple", ['kiwi', 'grapes',], "watermelon", ['potato', 'onion', ['sweet', 'laddu']]]
+// const another_array = ["banana","orange", "apple", ['kiwi', 'grapes',], "watermelon", ['potato', 'onion', ['sweet', 'laddu']]]
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array)
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array)
 
-console.log(Array.from("sujata"))
+// console.log(Array.isArray ("sujata")) 
+// console.log(Array.from("sujata")) 
+// console.log(Array.from({name: "sujata"}))  //intresting, converts to an array with a single element
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of (score1, score2, score3));
+
+const super_heros = ["tonystark", "Ironman", "spiderman"];
+
+console.log(super_heros.length); // length method 3
+super_heros.reverse(); // reverse method
+console.log("A",super_heros); // 
+
+const str = super_heros.join(" , "); // join method
+console.log(str);
---
+++ 03_objects.js
@@ -0,0 +1,39 @@
+//singleton
+//object.create
+
+//Object literal
+
+const mySym = Symbol('key1');
+
+const JsUser = {
+    name: "Sujata",
+    [mySym]: 'key1',
+    age: 23,
+    email: 'my@gmail.com',
+    hobbies: ['reading', 'traveling', 'swimming'],
+    address: 'India',
+    isLoggedIn: false,
+    lastLoginDays : ['Monday', 'Friday'
+
+    ]
+}
+
+// console.log(typeof JsUser.mySym);
+// console.log(JsUser[mySym]);
+
+JsUser.email = 'sujata@chatgpt.com' //updating email 
+// console.log(typeof JsUser.mySym)
+
+// Object.freeze(JsUser); //freeze the object, no updating allowed after this.
+JsUser.email = 'sujata@microsoft.com'
+// console.log(JsUser.email);
+
+JsUser.greeting = function(){
+    console.log('Hello Js User');
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js User ${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
