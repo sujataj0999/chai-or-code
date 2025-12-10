2d6ed004bd61f958be89405a6f8f8a153947826e
Sujata <sujataj0999@gmail.com>
2025-12-10 17:54:00 +0530

    new update

--- 04_function/07_iife.js
+++ 04_function/07_iife.js
@@ -2,12 +2,12 @@
 (function Car (){
     console.log(`DB Connected`);
 
-})();
+})();    //this is called named IIFE
 //Car()  // normal way to call a function
 
 
 // IIFE is used to create a new scope and avoid polluting the global scope
 
 ((name) => {
-    console.log(`DB Connected Two ${name}`);
-})('Sujata')
+    console.log(`DB Connected Two ${name}`);     //this is called simple IIFE
+})('Sujata')                                     // use ; semicolon between two IIFE functions to avoid errors
